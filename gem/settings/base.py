872b23ebd93887d8916d8901e4acbdffba0c1e9d
codie <codiebeulaine@gmail.com>
2017-03-01 13:41:44 +0200

    Update base.py

--- gem/settings/base.py
+++ gem/settings/base.py
@@ -118,6 +118,7 @@ MIDDLEWARE_CLASSES = [
     'molo.core.middleware.NoScriptGASessionMiddleware',
 
     'molo.core.middleware.MoloGoogleAnalyticsMiddleware',
+    'molo.core.middleware.MultiSiteRedirectToHomepage',
 ]
 
 # Template configuration
